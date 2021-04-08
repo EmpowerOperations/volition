ff488b51bb10fa771cd25eb558bd0e30dc2fb1a6
groostav <geoff.groos@empowerops.com>
2021-04-07 22:35:12 -0700

    well it builds, but it require some modifications to the generated source code.

---
+++ cpp-client-reference/Source.cpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "..\api\build\generated\source\proto\main\cpp\optimizer.pb.h"
+#include "..\api\build\generated\source\proto\main\grpc_cpp\optimizer.grpc.pb.h"
+
+
+int main() {
+
+	return 4;
+}

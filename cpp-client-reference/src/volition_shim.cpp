740890b038a29eda95ec2c619c49eb1b6fe61adc
vchlin <43309580+vchlin@users.noreply.github.com>
2024-05-04 13:01:27 -0700

    Re-design and implement Volition C API (#40)
    # Volition C API (Take Two)
    
    The API design in PR #39 was relatively high-level with lots of
    simplifications made to the Protobuf message hierarchy.
    
    This PR instead implements a slightly lower-level API with the primary
    goal being making the API more consistent and easier to mechanically
    translate without sacrificing type safety. The new design follows the
    Protobuf native C++ API more closely.
    
    ## API Design
    
    The API design makes extensive use of opaque handle types. The
    implementations are not exposed to the consumer of the API. Type safety
    and class invariants are preserved as the user can only interact with
    the API objects through pre-defined methods.
    
    ### API Objects
    
    The primary API objects include:
    
    | Type | Description |
    | :- | :- |
    | `unary_optimizer_t` | Represents a connection endpoint to the
    optimizer service. |
    | `start_optimization_command_t` | Contains the problem definition and
    various parameters to be passed to the optimizer. |
    | `optimizer_generated_query_stream_t` | The query stream returned by
    the optimizer service. |
    | `optimizer_generated_query_t` | A single query generated by the
    optimizer. |
    | `simulation_evaluation_completed_response_t` | Client-generated
    evaluation result corresponding to an evaluation request from the
    optimizer. |
    
    ## Misc Changes
    - Enable various [hardening
    options](https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html)
    and sanitizer flags for GCC and Clang builds.
    - Add `.clang-format` and `.clang-tidy` files.
    
    ## Next Steps
    - Move the API implementation code to another directory.
    - Update the reference client example to use pre-built Volition SDK
    artifacts.

--- cpp-client-reference/src/volition_shim.cpp
+++ cpp-client-reference/src/volition_shim.cpp
@@ -1,68 +1,304 @@
 #include "volition.h"
 
-struct vl_optimizer_s {};
+#include "optimizer.grpc.pb.h"
+#include "optimizer.pb.h"
 
-vl_optimizer vl_create_optimizer(char const* connection, vl_status* status) noexcept {
-    return nullptr;
+#include "grpcpp/grpcpp.h"
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+using namespace empowerops::volition;
+using namespace empowerops::volition::dto;
+using namespace google;
+
+#define GUARD(cond)      \
+    if (!(cond)) return; \
+    else static_cast<void>(0) // NOLINT(readability-else-after-return)
+#define GUARD_R(cond)       \
+    if (!(cond)) return {}; \
+    else static_cast<void>(0) // NOLINT(readability-else-after-return)
+
+template<class T>
+concept Pointer = std::is_pointer_v<T>;
+template<class T>
+concept NonPointer = !Pointer<T>;
+
+template<NonPointer T>
+constexpr auto into_unique(Pointer auto p) -> std::unique_ptr<T> {
+    return std::unique_ptr<T>{reinterpret_cast<T*>(p)};
 }
 
-void vl_destroy_optimizer(vl_optimizer optimizer) noexcept {}
-
-vl_input
-vl_create_continuous_input(char const* name, double lower_bound, double upper_bound) noexcept {
-    return nullptr;
+template<NonPointer T, Pointer U>
+constexpr auto allocate_opaque() noexcept -> U {
+    return reinterpret_cast<U>(std::make_unique<T>().release());
 }
 
-vl_input vl_create_step_input(
-    char const* name,
-    double lower_bound,
-    double upper_bound,
-    double step_size) noexcept {
-    return nullptr;
+auto continuous_set_lower_bound(continuous_t continuous, double lower_bound) noexcept -> void {
+    GUARD(continuous);
+    reinterpret_cast<ContinuousDTO*>(continuous)->set_lower_bound(lower_bound);
 }
 
-vl_evaluable vl_create_babel_constraint_evaluable(
-    char const* output_name,
-    char const* expr) noexcept {
-    return nullptr;
+auto continuous_set_upper_bound(continuous_t continuous, double upper_bound) noexcept -> void {
+    GUARD(continuous);
+    reinterpret_cast<ContinuousDTO*>(continuous)->set_upper_bound(upper_bound);
 }
 
-vl_evaluable vl_create_babel_scalar_evaluable(char const* output_name, char const* expr) noexcept {
-    return nullptr;
+auto input_parameter_set_name(input_parameter_t input, char const* name) noexcept -> void {
+    GUARD(input && name);
+    reinterpret_cast<InputParameterDTO*>(input)->set_name(name);
 }
 
-vl_evaluable vl_create_client_simulation_evaluable(
-    char const* name,
-    vl_client_input const* inputs,
-    size_t input_count,
-    vl_client_output const* outputs,
-    size_t output_count,
-    vl_duration timeout) noexcept {
-    return nullptr;
+auto input_parameter_set_continuous(input_parameter_t input) noexcept -> continuous_t {
+    GUARD_R(input);
+    auto* msg = reinterpret_cast<InputParameterDTO*>(input);
+    return reinterpret_cast<continuous_t>(msg->mutable_continuous());
 }
 
-struct vl_event_stream_s {};
-
-vl_event_stream vl_create_event_stream(
-    vl_optimizer optimizer,
-    vl_optimization_settings const* settings,
-    vl_problem_definition const* problem_definition,
-    vl_seed_point const* seed_points,
-    size_t point_count,
-    vl_status* status) noexcept {
-    return nullptr;
+auto variable_mapping_set_inputs(
+    variable_mapping_t mapping,
+    char const* simulation_name,
+    char const* optimization_name) noexcept -> void {
+    GUARD(mapping && simulation_name && optimization_name);
+    auto* map = reinterpret_cast<VariableMappingDTO*>(mapping)->mutable_inputs();
+    (*map)[simulation_name].set_value(optimization_name);
 }
 
-void vl_destroy_event_stream(vl_event_stream event_stream) noexcept {}
-
-vl_status vl_wait_event(vl_event_stream event_stream, vl_event_callbacks* callbacks) {
-    return VL_SUCCESS;
+auto variable_mapping_set_outputs(
+    variable_mapping_t mapping,
+    char const* simulation_name,
+    char const* optimization_name) noexcept -> void {
+    GUARD(mapping && simulation_name && optimization_name);
+    auto* map = reinterpret_cast<VariableMappingDTO*>(mapping)->mutable_outputs();
+    (*map)[simulation_name].set_value(optimization_name);
 }
 
-vl_status vl_stop_optimization(vl_event_stream events) noexcept { return VL_SUCCESS; }
+auto simulation_input_parameter_set_name(
+    simulation_input_parameter_t input,
+    char const* name) noexcept -> void {
+    GUARD(input && name);
+    reinterpret_cast<SimulationInputParameterDTO*>(input)->set_name(name);
+}
 
-vl_status vl_send_evaluation_result(
-    vl_evaluation_token token,
-    vl_evaluation_result const* result) noexcept {
-    return VL_SUCCESS;
+auto simulation_output_parameter_set_name(
+    simulation_output_parameter_t output,
+    char const* name) noexcept -> void {
+    GUARD(output && name);
+    reinterpret_cast<SimulationOutputParameterDTO*>(output)->set_name(name);
+}
+
+auto simulation_output_parameter_set_is_boolean(
+    simulation_output_parameter_t output,
+    bool boolean) noexcept -> void {
+    GUARD(output);
+    reinterpret_cast<SimulationOutputParameterDTO*>(output)->set_is_boolean(boolean);
+}
+
+auto simulation_node_set_mapping_table(simulation_node_t node) noexcept -> variable_mapping_t {
+    GUARD_R(node);
+    auto* msg = reinterpret_cast<SimulationNodeDTO*>(node);
+    return reinterpret_cast<variable_mapping_t>(msg->mutable_mapping_table());
+}
+
+auto simulation_node_add_inputs(simulation_node_t node) noexcept -> simulation_input_parameter_t {
+    GUARD_R(node);
+    auto* msg = reinterpret_cast<SimulationNodeDTO*>(node);
+    return reinterpret_cast<simulation_input_parameter_t>(msg->mutable_inputs()->Add());
+}
+
+auto simulation_node_add_outputs(simulation_node_t node) noexcept -> simulation_output_parameter_t {
+    GUARD_R(node);
+    auto* msg = reinterpret_cast<SimulationNodeDTO*>(node);
+    return reinterpret_cast<simulation_output_parameter_t>(msg->mutable_outputs()->Add());
+}
+
+auto evaluable_node_set_simulation(evaluable_node_t node) noexcept -> simulation_node_t {
+    GUARD_R(node);
+    auto* msg = reinterpret_cast<EvaluableNodeDTO*>(node);
+    return reinterpret_cast<simulation_node_t>(msg->mutable_simulation());
+}
+
+auto problem_definition_add_inputs(problem_definition_t problem) noexcept -> input_parameter_t {
+    GUARD_R(problem);
+    auto* msg = reinterpret_cast<ProblemDefinitionDTO*>(problem);
+    return reinterpret_cast<input_parameter_t>(msg->add_inputs());
+}
+
+auto problem_definition_add_evaluables(problem_definition_t problem) noexcept -> evaluable_node_t {
+    GUARD_R(problem);
+    auto* msg = reinterpret_cast<ProblemDefinitionDTO*>(problem);
+    return reinterpret_cast<evaluable_node_t>(msg->add_evaluables());
+}
+
+auto optimization_settings_set_iteration_count(
+    optimization_settings_t settings,
+    std::uint32_t count) noexcept -> void {
+    GUARD(settings);
+    reinterpret_cast<OptimizationSettingsDTO*>(settings)->set_iteration_count(count);
+}
+
+auto optimization_settings_set_concurrent_run_count(
+    optimization_settings_t settings,
+    std::uint32_t count) noexcept -> void {
+    GUARD(settings);
+    reinterpret_cast<OptimizationSettingsDTO*>(settings)->set_concurrent_run_count(count);
+}
+
+auto start_optimization_command_create() noexcept -> start_optimization_command_t {
+    return allocate_opaque<StartOptimizationCommandDTO, start_optimization_command_t>();
+}
+
+auto start_optimization_command_destroy(start_optimization_command_t command) noexcept -> void {
+    into_unique<StartOptimizationCommandDTO>(command);
+}
+
+auto start_optimization_command_set_problem_definition(
+    start_optimization_command_t command) noexcept -> problem_definition_t {
+    GUARD_R(command);
+    auto* msg = reinterpret_cast<StartOptimizationCommandDTO*>(command);
+    return reinterpret_cast<problem_definition_t>(msg->mutable_problem_definition());
+}
+
+auto start_optimization_command_set_settings(start_optimization_command_t command) noexcept
+    -> optimization_settings_t {
+    GUARD_R(command);
+    auto* msg = reinterpret_cast<StartOptimizationCommandDTO*>(command);
+    return reinterpret_cast<optimization_settings_t>(msg->mutable_settings());
+}
+
+auto unary_optimizer_create(char const* connection) noexcept -> unary_optimizer* try {
+    GUARD_R(connection);
+    auto channel = grpc::CreateChannel(connection, grpc::InsecureChannelCredentials());
+    return reinterpret_cast<unary_optimizer_t>(UnaryOptimizer::NewStub(channel).release());
+} catch (...) {
+    return {};
+}
+
+auto unary_optimizer_destroy(unary_optimizer_t optimizer) noexcept -> void {
+    into_unique<UnaryOptimizer::Stub>(optimizer);
+}
+
+auto simulation_evaluation_request_get_name(simulation_evaluation_request_t request) noexcept
+    -> char const* {
+    GUARD_R(request);
+    return reinterpret_cast<SimulationEvaluationRequestDTO*>(request)->name().c_str();
+}
+
+auto simulation_evaluation_request_get_iteration_index(
+    simulation_evaluation_request_t request) noexcept -> std::uint32_t {
+    GUARD_R(request);
+    return reinterpret_cast<SimulationEvaluationRequestDTO*>(request)->iteration_index();
+}
+
+auto simulation_evaluation_request_get_input_vector(
+    simulation_evaluation_request_t request,
+    char const* name) noexcept -> double {
+    GUARD_R(request);
+    return reinterpret_cast<SimulationEvaluationRequestDTO*>(request)->input_vector().at(name);
+}
+
+auto optimizer_generated_query_destroy(optimizer_generated_query_t query) noexcept -> void {
+    into_unique<OptimizerGeneratedQueryDTO>(query);
+}
+
+auto optimizer_generated_query_get_simulation_evaluation_request(
+    optimizer_generated_query_t query) noexcept -> simulation_evaluation_request_t {
+    GUARD_R(query);
+    auto* p = reinterpret_cast<OptimizerGeneratedQueryDTO*>(query);
+    if (p->has_evaluation_request())
+        return reinterpret_cast<simulation_evaluation_request_t>(p->mutable_evaluation_request());
+    return {};
+}
+
+struct optimizer_generated_query_stream {
+    std::unique_ptr<grpc::ClientReader<OptimizerGeneratedQueryDTO>> reader{};
+    grpc::ClientContext context{};
+};
+
+auto optimizer_generated_query_stream_destroy(optimizer_generated_query_stream_t stream) noexcept
+    -> void {
+    std::unique_ptr<optimizer_generated_query_stream>{stream};
+}
+
+auto optimizer_generated_query_stream_wait(optimizer_generated_query_stream_t stream) noexcept
+    -> optimizer_generated_query_t try {
+    GUARD_R(stream);
+    auto msg = std::make_unique<OptimizerGeneratedQueryDTO>();
+    if (stream->reader->Read(msg.get()))
+        return reinterpret_cast<optimizer_generated_query_t>(msg.release());
+    return {};
+} catch (...) {
+    return {};
+}
+
+auto unary_optimizer_start_optimization(
+    unary_optimizer_t optimizer,
+    start_optimization_command_t command) noexcept -> optimizer_generated_query_stream_t try {
+    GUARD_R(optimizer && command);
+    auto* stub = reinterpret_cast<UnaryOptimizer::Stub*>(optimizer);
+    auto stream = std::make_unique<optimizer_generated_query_stream>();
+    stream->reader = stub->StartOptimization(
+        &stream->context, *into_unique<StartOptimizationCommandDTO>(command));
+    return stream.release();
+} catch (...) {
+    return {};
+}
+
+auto output_vector_set_entries(output_vector_t output, char const* name, double value) noexcept
+    -> void {
+    GUARD(output && name);
+    (*reinterpret_cast<OutputVectorDTO*>(output)->mutable_entries())[name] = value;
+}
+
+auto simulation_evaluation_completed_response_create() noexcept
+    -> simulation_evaluation_completed_response_t {
+    return allocate_opaque<
+        SimulationEvaluationCompletedResponseDTO,
+        simulation_evaluation_completed_response_t>();
+}
+
+auto simulation_evaluation_completed_response_destroy(
+    simulation_evaluation_completed_response_t response) noexcept -> void {
+    into_unique<SimulationEvaluationCompletedResponseDTO>(response);
+}
+
+auto simulation_evaluation_completed_response_set_name(
+    simulation_evaluation_completed_response_t response,
+    char const* name) noexcept -> void {
+    GUARD(response && name);
+    reinterpret_cast<SimulationEvaluationCompletedResponseDTO*>(response)->set_name(name);
+}
+
+auto simulation_evaluation_completed_response_set_iteration_index(
+    simulation_evaluation_completed_response_t response,
+    uint32_t index) noexcept -> void {
+    GUARD(response);
+    reinterpret_cast<SimulationEvaluationCompletedResponseDTO*>(response)->set_iteration_index(
+        index);
+}
+
+auto simulation_evaluation_completed_response_set_abort_optimization(
+    simulation_evaluation_completed_response_t response,
+    bool abort) noexcept -> void {
+    GUARD(response);
+    reinterpret_cast<SimulationEvaluationCompletedResponseDTO*>(response)->set_abort_optimization(
+        abort);
+}
+
+auto simulation_evaluation_completed_response_set_vector(
+    simulation_evaluation_completed_response_t response) noexcept -> output_vector_t {
+    GUARD_R(response);
+    auto* msg = reinterpret_cast<SimulationEvaluationCompletedResponseDTO*>(response);
+    return reinterpret_cast<output_vector_t>(msg->mutable_vector());
+}
+
+auto unary_optimizer_offer_simulation_result(
+    unary_optimizer_t optimizer,
+    simulation_evaluation_completed_response_t response) noexcept -> void {
+    GUARD(optimizer && response);
+    auto context = grpc::ClientContext{};
+    auto confirm = SimulationEvaluationResultConfirmDTO{};
+    reinterpret_cast<UnaryOptimizer::Stub*>(optimizer)->OfferSimulationResult(
+        &context, *into_unique<SimulationEvaluationCompletedResponseDTO>(response), &confirm);
 }
--- cpp-client-reference/test/test_c99.c
+++ cpp-client-reference/test/test_c99.c
@@ -1,72 +1,78 @@
 #include "volition.h"
 
-#include <math.h>
 #include <stdio.h>
 
-#define COUNT(arr) (sizeof(arr) / sizeof(arr[0]))
-
-typedef struct {
-    double last_value;
-} simulation_state;
-
-void on_evaluation_request(void* user_data, vl_evaluation_token token, double const* input) {
-    simulation_state* state = (simulation_state*)user_data;
-    double const x1 = input[0];
-    vl_evaluation_result const result = {
-        .output = (double[]){x1 * x1 * state->last_value},
-    };
-    vl_send_evaluation_result(token, &result);
-    state->last_value -= 0.1;
-}
-
-void on_optimization_complete(void* user_data, vl_completion_token token) {
-    printf("optimization complete.\n");
-}
-
 int main() {
-    vl_optimizer const optimizer = vl_create_optimizer("localhost:27016", NULL);
-    if (!optimizer) return -1;
-    vl_optimization_settings const settings = {
-        .target_objective_value = -INFINITY,
-        .concurrent_run_count = 2,
-        .iteration_count = 5,
-    };
-    vl_input const inputs[] = {vl_create_continuous_input("x1", 0.5, 5.0)};
-    vl_client_input const sim_inputs[] = {{.name = "x1", .client_name = "x1"}};
-    vl_client_output const sim_outputs[] = {{.name = "f1", .client_name = "f1", .is_bool = false}};
-    vl_evaluable const evaluables[] = {vl_create_client_simulation_evaluable(
-        "test",
-        sim_inputs,
-        COUNT(sim_inputs),
-        sim_outputs,
-        COUNT(sim_outputs),
-        (vl_duration){.seconds = 0})};
-    vl_problem_definition const problem = {
-        .inputs = inputs,
-        .input_count = COUNT(inputs),
-        .evaluables = evaluables,
-        .evaluable_count = COUNT(evaluables),
-    };
-    vl_seed_point seed_points[] = {
-        {.input = (double[]){0.6}, .output = (double[]){42.0}},
-        {.input = (double[]){0.7}, .output = (double[]){42.0}},
-    };
-    vl_event_stream const event_stream = vl_create_event_stream(
-        optimizer, &settings, &problem, seed_points, COUNT(seed_points), NULL);
-    if (!event_stream) return -2;
-    simulation_state state = {
-        .last_value = 42.0,
-    };
-    vl_event_callbacks callbacks = {
-        .user_data = &state,
-        .on_evaluation_request = on_evaluation_request,
-        .on_optimization_complete = on_optimization_complete,
-    };
-    vl_status status;
-    while (!(status = vl_wait_event(event_stream, &callbacks))) {}
-    if (status != VL_STREAM_FINISHED) return -3;
+    printf("Simulation client started.\n");
 
-    vl_destroy_event_stream(event_stream);
-    vl_destroy_optimizer(optimizer);
+    start_optimization_command_t start = start_optimization_command_create();
+
+    problem_definition_t problem = start_optimization_command_set_problem_definition(start);
+
+    input_parameter_t input = problem_definition_add_inputs(problem);
+    input_parameter_set_name(input, "x1");
+    continuous_t cont = input_parameter_set_continuous(input);
+    continuous_set_lower_bound(cont, 0.5);
+    continuous_set_upper_bound(cont, 5.0);
+
+    evaluable_node_t evaluable = problem_definition_add_evaluables(problem);
+    simulation_node_t sim = evaluable_node_set_simulation(evaluable);
+    variable_mapping_t mapping = simulation_node_set_mapping_table(sim);
+    variable_mapping_set_inputs(mapping, "x1", "x1");
+    variable_mapping_set_outputs(mapping, "f1", "f1");
+    simulation_input_parameter_t sim_input = simulation_node_add_inputs(sim);
+    simulation_input_parameter_set_name(sim_input, "x1");
+    simulation_output_parameter_t sim_output = simulation_node_add_outputs(sim);
+    simulation_output_parameter_set_name(sim_output, "f1");
+
+    optimization_settings_t settings = start_optimization_command_set_settings(start);
+    optimization_settings_set_iteration_count(settings, 5);
+    // TODO: Update example with multi-threaded usage?
+    // optimization_settings_set_concurrent_run_count(settings, 2);
+
+    unary_optimizer_t optimizer = unary_optimizer_create("localhost:27016");
+    if (!optimizer) {
+        printf("Could not connect to optimizer.\n");
+        start_optimization_command_destroy(start);
+        return -1;
+    }
+
+    optimizer_generated_query_stream_t stream =
+        unary_optimizer_start_optimization(optimizer, start);
+
+    double last_value = 42.0;
+    optimizer_generated_query_t query = NULL;
+    while ((query = optimizer_generated_query_stream_wait(stream))) {
+        simulation_evaluation_request_t evaluation_request =
+            optimizer_generated_query_get_simulation_evaluation_request(query);
+        if (evaluation_request) {
+            printf("Received evaluation request.\n");
+
+            uint32_t index = simulation_evaluation_request_get_iteration_index(evaluation_request);
+            double x1 = simulation_evaluation_request_get_input_vector(evaluation_request, "x1");
+            printf("x1 is %f\n", x1);
+
+            simulation_evaluation_completed_response_t response =
+                simulation_evaluation_completed_response_create();
+            simulation_evaluation_completed_response_set_iteration_index(response, index);
+            output_vector_t output = simulation_evaluation_completed_response_set_vector(response);
+            double f1 = last_value * x1;
+            printf("f1 is %f\n", f1);
+            output_vector_set_entries(output, "f1", f1);
+            last_value -= 0.1;
+
+            unary_optimizer_offer_simulation_result(optimizer, response);
+            printf("Sent result.\n");
+            continue;
+        }
+        printf("Skipping unknown request.\n");
+        optimizer_generated_query_destroy(query);
+    }
+    printf("Channel closed.\n");
+
+    optimizer_generated_query_stream_destroy(stream);
+
+    unary_optimizer_destroy(optimizer);
+
     return 0;
 }
